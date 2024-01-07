e9ee9bcf7dabc9acec556f233ff1e20b90a03c22
Mojammel Molla <Mojammelmolla897@gmail.com>
2024-01-08 01:10:39 +0600

    Footer section created with responsive

--- src/lay-outs/main-layout/MainLayout.jsx
+++ src/lay-outs/main-layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../../pages/home/navbar/Navbar';
+import Footer from '../../pages/home/footer/Footer';
 
 const MainLayout = () => {
   return (
     <>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </>
   );
 };
